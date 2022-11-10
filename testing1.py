3ff9d4e4aa1f78a38a6396769ea8839ad2f55fb1
Meirion <hillsmt@cardiff.ac.uk>
2022-11-10 11:36:23 +0000

    Initial save
    First

---
+++ testing1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 11:34:29 2022
+
+@author: c1216647
+"""
+
+#Testing for GIT
+
+print("Hello World")
+print("Type your name")
+name=input()
+
+print("Hi",name)
